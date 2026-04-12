81eb83918881689a59b637a1913d69546abdd42e
Павел Неустроев <n3@MacBook-Air-Pavel-2.local>
2026-04-12 17:22:42 +0300

    docs: setup GitHub Pages deployment

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/BlackWorkInc/',
   server: {
     port: 1574,
     host: true
